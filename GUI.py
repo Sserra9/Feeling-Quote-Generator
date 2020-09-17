024c0fad612181455bf90a3d0d896bd02978278f
Sean Serra <69557379+Sserra9@users.noreply.github.com>
2020-09-16 21:19:07 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,11 +2,12 @@
 from tkinter import * 
 import tkinter as tk
 
-
+#generates a display window.
 root = Tk()
 root.geometry("800x600")
 root.resizable(height = 0, width = 0)
 
+#defines the text entry box, placeholder text, and grid for the display
 e = Entry(root, width = 75, fg = "black", bg = "white", font=("Arial",12))
 e.grid(row=22, column=2, columnspan=4, padx=25, pady=10)
 e.insert(0, "Type to get some inspiration: ")
@@ -14,10 +15,9 @@ e.insert(0, "Type to get some inspiration: ")
 text1 = tk.Label(text='Want a quote to match how you are feeling or how you want to feel?', font=("Arial",18))
 text1.grid(row=10, column=2, padx=25, pady=10)
 
-
+#prints instructions and a list of feelings to the display.
 text2 = tk.Label(text='Type a feeling from the list in the text box below.', font=("Arial",15))
 text2.grid(row=11, column=2, padx=25, pady=10)
-
 
 text3= tk.Label(text= 'Happy 😀', font=("Arial",15))
 text3.grid(row=13, column=2, padx=25, pady=10)
@@ -39,13 +39,4 @@ text11= tk.Label(text= 'Successful 🤩')
 text11.grid(row=21, column=2, padx=25, pady=10)
 
 
-#quote = 'you are the greatest'
-
-#def my_click():
-#    my_label = Label(root, text = quote)
-#    my_label.grid(row=3, column=5, columnspan=3, padx=5, pady=10)
-#
-#my_button = Button(root, text='Click Me!', padx=50, pady=50, command=my_click, fg='yellow', bg='green', state=NORMAL)
-#my_button.grid(row=3, column=5, columnspan=3, padx=5, pady=10)
-#
 root.mainloop()
